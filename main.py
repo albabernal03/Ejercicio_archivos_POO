a352d0df1b61bbb3204205b8a7c227791883567f
albabernal03 <ALBABERO@MYUAX.COM>
2022-05-03 19:48:39 +0200

    terminamos crear el main

---
+++ main.py
@@ -0,0 +1,9 @@
+from clases.ejercicio import calificaciones
+from clases.ejercicio import nota_final
+from clases.ejercicio import aprobados_suspensos
+
+if __name__ == '__main__':
+    calificaciones()
+    nota_final()
+    aprobados_suspensos()
+    
